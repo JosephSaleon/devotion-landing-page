5dc375b61b97ea7e2519045aeac455bb08f1ca30
JosephSaleon <joseph.saleonterras@gmail.com>
2024-07-17 13:02:08 +0200

    Rename Blog to Demos

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -5,12 +5,12 @@ import { cn } from "@lib/utils"
 
 type Props = {
   tags: string[]
-  data: CollectionEntry<"blog">[]
+  data: CollectionEntry<"demos">[]
 }
 
 export default function Blog({ data, tags }: Props) {
   const [filter, setFilter] = createSignal(new Set<string>())
-  const [posts, setPosts] = createSignal<CollectionEntry<"blog">[]>([])
+  const [posts, setPosts] = createSignal<CollectionEntry<"demos">[]>([])
 
   createEffect(() => {
     setPosts(data.filter((entry) => 
--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -1,27 +1,27 @@
-import { getCollection } from "astro:content";
+import { getCollection } from "astro:content"
 
 export async function getCategories() {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("demos")
 
   const categories = [
     ...new Set([].concat.apply(posts.map((post) => post.data.category))),
-  ];
+  ]
 
-  return categories;
+  return categories
 }
 
 export async function getPosts() {
-  const posts = (await getCollection("blog")).sort(
+  const posts = (await getCollection("demos")).sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-  );
+  )
 
-  return posts;
+  return posts
 }
 
 export async function getPostsByCategory(category: string) {
-  const posts = (await getCollection("blog"))
+  const posts = (await getCollection("demos"))
     .filter((post) => post.data.category === category)
-    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
 
-  return posts;
+  return posts
 }
